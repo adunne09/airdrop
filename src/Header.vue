9202c6703a1dc6d4642c164092b32aa2b81fc1e0
Alex Dunne <alex12dunne@gmail.com>
2022-04-17 23:19:25 -0400

    tweak Header styles

--- src/Header.vue
+++ src/Header.vue
@@ -1,17 +1,21 @@
 <template>
   <header class="w-full flex justify-center my-8">
-    <div>
+    <div class="flex-1 mb-7 border-b-2 border-pink-500" />
+    <div class="flex-1">
       <router-link to="/" class="text-4xl font-bold hover:text-pink-500">
         Airdrop Bazaar
       </router-link>
-      <div class="flex mt-4 items-center border-2 border-pink-500 rounded-lg">
+      <div
+        class="flex justify-around mt-4 items-center border-2 border-pink-500 rounded-lg"
+      >
         <HeaderLink to="/create-item" title="Create Item" />
-        <div class="border-x-2 border-pink-500">
+        <div class="border-x-2 border-pink-500 px-4">
           <HeaderLink to="/unclaimed-items" title="Unclaimed Items" />
         </div>
         <HeaderLink to="/created-items" title="Created Items" />
       </div>
     </div>
+    <div class="flex-1 mb-7 border-b-2 border-pink-500" />
   </header>
 </template>
 
