9cc2c9360d6a19a04ace1bf9fafcd838109124ec
Alex Dunne <alex12dunne@gmail.com>
2022-04-24 15:07:11 -0400

    textarea component

---
+++ src/components/Textarea.vue
@@ -0,0 +1,24 @@
+<template>
+  <textarea
+    class="border-2 border-pink-500 rounded p-1 resize-none"
+    :value="modelValue"
+    @input="handleChange($event)"
+    :disabled="disabled"
+  />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  modelValue: string
+  disabled?: boolean
+}>()
+
+const emits = defineEmits<{
+  (e: 'update:modelValue', value: string | number): void
+}>()
+
+const handleChange = (e: Event) =>
+  emits('update:modelValue', (e.target as HTMLTextAreaElement).value)
+</script>
+
+<style scoped></style>
