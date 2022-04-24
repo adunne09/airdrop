4e7c8769f2cf68349e1a0e66ea68c3b40bd2bf35
Alex Dunne <alex12dunne@gmail.com>
2022-04-24 15:03:03 -0400

    file input component

---
+++ src/components/FileInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <label
+    :for="id"
+    :class="[
+      'font-bold py-1 bg-pink-500 text-white rounded border-2 border-pink-500 hover:bg-white hover:text-pink-500',
+      { 'cursor-pointer': !disabled },
+      { 'cursor-not-allowed': disabled },
+    ]"
+  >
+    <input :id="id" type="file" class="hidden" v-bind="$attrs" />
+    <slot />
+  </label>
+</template>
+
+<script setup lang="ts">
+import { v4 } from 'uuid'
+
+defineProps<{
+  disabled?: boolean
+}>()
+
+const id = v4()
+</script>
+
+<style scoped></style>
