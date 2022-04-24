dd5175c2f0a66c1ef79527e0f957552ad6d0fba5
Alex Dunne <alex12dunne@gmail.com>
2022-04-24 16:12:46 -0400

    add loader to button component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -2,7 +2,7 @@
   <button
     type="button"
     :class="[
-      'w-full font-bold rounded px-2 py-1',
+      'w-full font-bold rounded px-2 py-1 flex justify-center',
       {
         'border-2 bg-pink-500 text-white cursor-pointer hover:bg-white  border-pink-500 hover:text-pink-500':
           kind === 'primary' && !disabled,
@@ -17,11 +17,14 @@
       { 'border-2 text-gray-400 cursor-not-allowed': disabled },
     ]"
   >
-    <slot />
+    <LoaderIcon v-if="loading" class="animate-spin h-5 w-5 text-pink-500" />
+    <slot v-else />
   </button>
 </template>
 
 <script setup lang="ts">
+import LoaderIcon from '@/assets/loader.svg?component'
+
 withDefaults(
   defineProps<{
     kind?: 'primary' | 'seconday' | 'text'
