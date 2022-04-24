0d36d408f23bb0c2d90748e8ab4c1b3ebd65daa0
Alex Dunne <alex12dunne@gmail.com>
2022-04-24 15:14:29 -0400

    refactor Home view to use components, tweak styling, use upload file input to decrypt encrypted items

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <ProgressBar v-if="state.loading" :loading="state.loading" class="w-96" />
   <div v-else>
-    <h2 v-if="!state.items.length">No Items Found.</h2>
+    <h2 v-if="!state.items.length">No Unclaimed Items Found.</h2>
     <div
       v-else
       class="grid gap-4 overflow-y-auto"
@@ -25,91 +25,59 @@
             </p>
           </div>
         </div>
-        <div class="p-4 bg-black">
-          <button
+        <div class="flex p-4 bg-black">
+          <Button
+            kind="primary"
             v-if="!item.claimed"
-            type="button"
-            class="w-full text-green-600 bg-white font-bold py-2 px-12 rounded hover:bg-green-600 hover:text-white"
             @click="handleClaimItem(item.tokenId)"
           >
             Unclaimed
-          </button>
-          <button
+          </Button>
+          <Button
+            kind="primary"
             v-else-if="item.claimed"
-            class="w-full text-green-600 bg-white font-bold py-2 px-12 rounded hover:bg-green-600 hover:text-white"
             @click="handleDownloadItem(item)"
           >
             Download
-          </button>
+          </Button>
         </div>
       </div>
     </div>
   </div>
-  <Modal v-if="state.selectedItemId" @close="state.selectedItemId = null">
-    <div style="width: 500px">
+  <Modal v-if="state.itemIdToDecrypt" @close="state.itemIdToDecrypt = null">
+    <div style="width: 300px">
       <h2 class="flex flex-start font-bold pb-2">Decrypt file</h2>
-      <form @submit.prevent="handleDownloadEncryptedItem()">
+      <form @submit.prevent="handleDownloadEncryptedItem()" class="grid gap-4">
         <ProgressBar
           v-if="state.loading"
           :loading="state.loading"
           class="absolute top-60 w-4/5"
         />
 
-        <div :class="['grid gap-4 w-full', { 'opacity-30': state.loading }]">
-          <div class="grid gap-2 pb-2 border-b">
-            <textarea
-              v-model="state.formValues.recipientPrivateKey"
-              name="private-key"
-              placeholder="Your private key"
-              class="border-2 border-pink-500 rounded p-1 resize-none"
-              style="height: 300px"
-            />
-            <input
-              v-model="state.formValues.recipientPassphrase"
-              name="keyPassphrase"
-              placeholder="Passphrase"
-              class="border-2 border-pink-500 rounded p-1"
-            />
-          </div>
-          <textarea
-            v-model="state.formValues.senderPublicKey"
-            name="public-key"
-            placeholder="The sender's public key"
-            class="border-2 border-pink-500 rounded p-1 resize-none"
-            style="height: 300px"
-          />
-        </div>
+        <FileInput class="w-full" @input="handleSelectDecryptionKey($event)">
+          Select Decryption Key
+        </FileInput>
 
-        <div class="flex justify-between pt-4">
-          <button
-            type="button"
-            class="font-bold rounded p-1 px-2 bg-pink-500 text-white cursor-pointer hover:bg-white border-2 border-pink-500 hover:text-pink-500"
-            @click=""
+        <Input
+          v-if="state.formValues.recipientKeyfile"
+          v-model="state.formValues.recipientPassphrase"
+          name="key-passphrase"
+          placeholder="Passphrase"
+          type="password"
+          autofocus
+        />
+
+        <div class="flex">
+          <Button kind="text" @click="state.itemIdToDecrypt = null">
+            Cancel
+          </Button>
+          <Button
+            type="submit"
+            kind="primary"
+            :disabled="!state.formValues.recipientPassphrase.length"
           >
-            Upload Key
-          </button>
-
-          <div class="flex gap-4">
-            <button
-              type="button"
-              @click="state.selectedItemId = null"
-              class="font-bold text-pink-500 cursor-pointer"
-            >
-              Cancel
-            </button>
-            <button
-              type="submit"
-              :disabled="!state.encryptedDownloadIsReady"
-              :class="[
-                'rounded px-2',
-                state.encryptedDownloadIsReady
-                  ? 'bg-pink-500 text-white cursor-pointer hover:bg-white hover:border-2 hover:border-pink-500 hover:text-pink-500'
-                  : 'border-2 border-gray-400 text-gray-400 cursor-not-allowed',
-              ]"
-            >
-              Decrypt & Download
-            </button>
-          </div>
+            Decrypt
+          </Button>
         </div>
       </form>
     </div>
@@ -118,17 +86,17 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, computed } from '@vue/runtime-core'
+import { onMounted } from '@vue/runtime-core'
 import { ref, Ref } from 'vue'
 import { BigNumber, ethers } from 'ethers'
 import axios from 'axios'
 import ProgressBar from '@/components/ProgressBar.vue'
 import Modal from '@/components/Modal.vue'
+import FileInput from '@/components/FileInput.vue'
+import Input from '@/components/Input.vue'
+import Button from '@/components/Button.vue'
 import * as openpgp from 'openpgp'
-import {
-  establishConnectionAndGetAirdropContract,
-  registerAccountChangeHandler,
-} from '@/utils'
+import { ethUtils, fileUtils } from '../utils'
 
 interface AirdropBlockchainItem {
   tokenId: BigNumber
@@ -146,8 +114,7 @@ interface Item {
   file: string
   claimed: boolean
   role: 'sender' | 'recipient'
-  isEncrypted: boolean
-  fileExtension: string
+  senderPublicKey?: openpgp.Key
 }
 
 interface State {
@@ -155,32 +122,23 @@ interface State {
   signer?: ethers.providers.JsonRpcSigner
   items: Item[]
 
-  selectedItemId: number | null
+  itemIdToDecrypt: number | null
   formValues: {
-    recipientPrivateKey: string
+    recipientKeyfile?: File
     recipientPassphrase: string
-    senderPublicKey: string
   }
-  encryptedDownloadIsReady: boolean
 }
 
 const state: Ref<State> = ref({
   loading: false,
   items: [],
 
-  selectedItemId: null,
+  itemIdToDecrypt: null,
   formValues: {
-    recipientPrivateKey: '',
+    recipientKey: null,
     recipientPassphrase: '',
-    senderPublicKey: '',
+    senderKey: null,
   },
-  encryptedDownloadIsReady: computed(() =>
-    Boolean(
-      state.value.formValues.recipientPrivateKey?.length &&
-        state.value.formValues.recipientPassphrase?.length &&
-        state.value.formValues.senderPublicKey?.length
-    )
-  ),
 })
 
 const downloadItemAnchor: Ref<HTMLAnchorElement | null> = ref(null)
@@ -190,7 +148,7 @@ const loadItems = async () => {
 
   try {
     const { address, contract: airdropContract } =
-      await establishConnectionAndGetAirdropContract()
+      await ethUtils.establishConnectionAndGetAirdropContract()
 
     const data = await airdropContract.fetchItems()
 
@@ -199,13 +157,20 @@ const loadItems = async () => {
         const tokenUri = await airdropContract.tokenURI(item.tokenId)
         const meta = await axios.get(tokenUri)
 
+        let senderPublicKey
+        if (meta.data.senderPublicKey) {
+          senderPublicKey = await openpgp.readKey({
+            armoredKey: meta.data.senderPublicKey,
+          })
+        }
+
         return {
           ...item,
           tokenId: item.tokenId.toNumber(),
           name: meta.data.name,
           description: meta.data.description,
           file: meta.data.file,
-          isEncrypted: meta.data.isEncrypted,
+          senderPublicKey,
         }
       })
     )
@@ -222,45 +187,66 @@ const loadItems = async () => {
   }
 }
 
-registerAccountChangeHandler(() => loadItems())
+ethUtils.registerAccountChangeHandler(() => loadItems())
 
 onMounted(() => loadItems())
 
 const handleDownloadItem = (item: Item) => {
-  if (item.isEncrypted) {
-    state.value.selectedItemId = item.tokenId
+  if (item.senderPublicKey) {
+    state.value.itemIdToDecrypt = item.tokenId
     return
   }
 
-  downloadItemAnchor.value!.href = item.file
-  downloadItemAnchor.value!.download = item.name
-  downloadItemAnchor.value!.click()
+  if (!downloadItemAnchor.value) {
+    console.error('failed to download item; something went wrong')
+    return
+  }
+
+  downloadItemAnchor.value.href = item.file
+  downloadItemAnchor.value.download = item.name
+  downloadItemAnchor.value.click()
+}
+
+const handleSelectDecryptionKey = (e: Event) => {
+  const target = e.target as HTMLInputElement
+  const file = (target.files as FileList)[0]
+  state.value.formValues.recipientKeyfile = file
 }
 
 const handleDownloadEncryptedItem = async () => {
   try {
+    if (!state.value.formValues.recipientKeyfile) {
+      throw new Error(
+        'failed to download encypted item; recipient private key file not selected'
+      )
+    }
+
     const item = state.value.items.find(
-      ({ tokenId }) => tokenId === state.value.selectedItemId
+      ({ tokenId }) => tokenId === state.value.itemIdToDecrypt
     )!
 
     const message = await openpgp.readMessage({
       armoredMessage: item.file,
     })
 
-    const publicKey = await openpgp.readKey({
-      armoredKey: state.value.formValues.senderPublicKey,
-    })
-    const privateKey = await openpgp.decryptKey({
-      privateKey: await openpgp.readPrivateKey({
-        armoredKey: state.value.formValues.recipientPrivateKey,
-      }),
-      passphrase: state.value.formValues.recipientPassphrase, // FIXME--
+    const privateKeyStartIdentifier = '-----BEGIN PGP PRIVATE KEY BLOCK-----'
+    const privateKeyEndIdentifier = '-----END PGP PRIVATE KEY BLOCK-----'
+
+    const privateKeyRaw = (await fileUtils.parseStringFromFile(
+      state.value.formValues.recipientKeyfile,
+      privateKeyStartIdentifier,
+      privateKeyEndIdentifier
+    )) as string
+
+    const senderPrivateKey = await openpgp.decryptKey({
+      privateKey: await openpgp.readPrivateKey({ armoredKey: privateKeyRaw }),
+      passphrase: state.value.formValues.recipientPassphrase,
     })
 
     const { data, signatures } = await openpgp.decrypt({
       message,
-      verificationKeys: publicKey,
-      decryptionKeys: privateKey,
+      verificationKeys: item.senderPublicKey,
+      decryptionKeys: senderPrivateKey,
     })
 
     const isValidSignature = await signatures[0].verified
@@ -273,13 +259,12 @@ const handleDownloadEncryptedItem = async () => {
     // if they cancel, they don't have to put in the keys again
 
     downloadItemAnchor.value!.href = base64String
-
-    downloadItemAnchor.value!.download = item.name // .{itme.fileExtension}
+    downloadItemAnchor.value!.download = item.name
     downloadItemAnchor.value!.click()
 
-    state.value.selectedItemId = null
+    state.value.itemIdToDecrypt = null
   } catch (e) {
-    console.log('failed to download item;', e)
+    console.error('failed to download item;', e)
   }
 }
 
@@ -288,7 +273,7 @@ const handleClaimItem = async (id: number) => {
 
   try {
     const { contract: airdropContract } =
-      await establishConnectionAndGetAirdropContract()
+      await ethUtils.establishConnectionAndGetAirdropContract()
 
     const transaction = await airdropContract.claimItem(id)
     await transaction.wait()
