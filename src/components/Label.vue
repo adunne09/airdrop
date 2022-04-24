e990b7f034b0c2460b3702910019d1706e832484
Alex Dunne <alex12dunne@gmail.com>
2022-04-24 15:06:00 -0400

    label component

---
+++ src/components/Label.vue
@@ -0,0 +1,17 @@
+<template>
+  <label
+    :for="labelFor"
+    class="text-pink-500 ease-out text-sm leading-2 tracking-widest font-semibold uppercase"
+  >
+    <slot />
+  </label>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  disabled?: boolean
+  labelFor?: string
+}>()
+</script>
+
+<style scoped></style>
